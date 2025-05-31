bd3afd27bd6f6c7b4fd02becb78e094705f07427
fremov <59342062+fremov@users.noreply.github.com>
2025-06-01 00:42:26 +0300

    first commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    printf("\nhello, world!\n");
+    printf("hello, world!\n");
+    printf("hello, world!\n");
+    printf("hello, world!\n");
+    int a= 32;
+    int b= 123;
+    short sdf =123;
+return 0;    
+}
+
