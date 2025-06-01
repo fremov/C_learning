a05a62771d25f0d3352cd606852929ac01028768
fremov <59342062+fremov@users.noreply.github.com>
2025-06-01 10:58:23 +0300

    sdfsdf

--- main.c
+++ main.c
@@ -1,15 +1,8 @@
 #include <stdio.h>
-int main()
-{
-    printf("\nhello, world!\n");
-    printf("hello, world!\n");
-    printf("hello, world!\n");
-    printf("hello, world!\n");
-    int a= 32;
-    int b= 123;
-    short sdf =123;
-    int dsff = 234;
-    int dsfrtt = 3244;
-return 0;    
-}
+int main() {
+  printf("hello, world!\n");
+  printf("hello, world!\n");
+  printf("ddff");
 
+  return 0;
+}
