7ab391205eeaf73737119b6496dc434dc63f3fec
fremov <59342062+fremov@users.noreply.github.com>
2025-06-02 22:21:13 +0300

    1.2

--- main.c
+++ main.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
-/*
- *
- * Вывод таблицы температур по Цельсию и по Фаренгейту
- */
 int main() {
-  int facr;
-  puts("Celsius\tFarengeit");
-  for (facr = 0; facr <= 300; facr += 20) {
-    printf("%3d\t%6.1f\n", facr, (5.0 / 9.0) * (facr - 32));
-  }
+  int c;
+  printf("%d", EOF);
+  while ((c = getchar()) != EOF)
+    putchar(c);
   return 0;
 }
